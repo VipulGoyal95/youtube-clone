9dba50f9f5aad031cb4f3deb7aa1865563a8375b
vipul goyal <vipulgoyal776@gmail.com>
2024-01-20 22:26:13 +0530

    second commit

---
+++ src/components/categoriesbar/CategoriesBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./categoriesbar.scss";
+const CategoriesBar = () => {
+  return (
+    <div>
+      categorie
+    </div>
+  )
+}
+
+export default CategoriesBar;
